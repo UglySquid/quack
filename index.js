ec7cb5c310abff3333cccfd05703553cb03ed77e
UglySquid <christine.wei.63@gmail.com>
2023-03-03 23:57:56 -0500

    dying from tetris and also now there's a map

---
+++ index.js
@@ -0,0 +1,9 @@
+import { useMemo } from "react";
+import { GoogleMap, useLoadScript, Marker } from "@react-google-map/api";
+
+export default function Home() {
+    const {}
+
+    return <div>Map</div>;
+}
+
